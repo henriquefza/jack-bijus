0b8d3bf32d67f39cd5a3944fd5b4462720b7dc3b
Henrique Feitoza da Silva <henriquesilva@mp.am.gov.br>
2020-11-10 16:24:47 -0400

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Home(){
+  return(
+  <div>
+    <h1>Jack Bijus</h1>
+    <a href="https://api.whatsapp.com/send?phone=5592985075503&text=Gostaria%20de%20adquirir%20um%20produto.%20%20"> WhatsApp</a>
+  </div>
+  )}
+export default Home;
